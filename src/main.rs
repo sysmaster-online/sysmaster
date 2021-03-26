8573f684dcca187bee246352d77c990f234878f6
Shinwell Hu <huxinwei@huawei.com>
2021-03-26 03:49:45 +0000

    signal handling initial test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use std::io::Error;
+
+use signal_hook::consts::signal::SIGCHLD;
+use signal_hook::iterator::SignalsInfo;
+use signal_hook::iterator::exfiltrator::WithOrigin;
+
+fn main() -> Result<(), Error> {
+    let sigs = vec![SIGCHLD];
+    
+    let mut signals = SignalsInfo::<WithOrigin>::new(&sigs)?;
+
+    for info in &mut signals {
+        println!("Received a signal {:?}", info);
+        #[cfg(feature="extended-siginfo")]
+        match info.signal {
+            SIGCHLD => {
+                println!("CHLD received");
+            }
+        }
+    }
     println!("Hello, world!");
+    Ok(())
 }
