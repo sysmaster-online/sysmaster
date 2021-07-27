4e575e2ef5dde7348b64e59244bebdd2b2b75284
Shinwell Hu <huxinwei@huawei.com>
2021-07-27 10:38:16 +0000

    execute command or standalone

--- src/main/src/main.rs
+++ src/main/src/main.rs
@@ -8,6 +8,29 @@ use signal_hook::consts::signal::SIGCHLD;
 use signal_hook::iterator::SignalsInfo;
 use signal_hook::iterator::exfiltrator::WithOrigin;
 
+
+#[derive(Debug)]
+enum Proc1Error {
+    IOError(std::io::Error),
+    NoOption,
+    ChildPidTooBig(u32, std::num::TryFromIntError),
+}
+
+impl std::convert::From<std::io::Error> for Proc1Error {
+    fn from(e: std::io::Error) -> Self {
+        Proc1Error::IOError(e)
+    }
+}
+
+fn get_command() -> Result<(String, Vec<String>), Proc1Error> {
+    let mut args = std::env::args();
+    let _me = args.next();
+    match args.next() {
+        None => Err(Proc1Error::NoOption),
+        Some(cmd) => Ok((cmd, args.collect())),
+    }
+}
+
 fn parse_inittab() -> Result<(), Error> {
     let inittab_file = File::open("/etc/inittab")?;
     let buf_reader = std::io::BufReader::new(inittab_file);
@@ -26,7 +49,26 @@ fn parse_inittab() -> Result<(), Error> {
     return Ok(())
 }
 
-fn main() -> Result<(), Error> {
+
+fn main() -> Result<(), Proc1Error> {
+    match (get_command()) {
+        Ok((cmd, args)) => execute_mode(cmd, args),
+        Err(Proc1Error::NoOption) => standalone_mode(),
+        Err(_) => Ok(()),
+    };
+
+    Ok(())
+}
+
+fn execute_mode(s: String, ar: Vec<String>) -> Result<(), Proc1Error> {
+    println!("running to execute specific command");
+    println!("{:?}", s);
+    println!("{:?}", ar);
+    return Ok(())
+}
+
+fn standalone_mode() -> Result<(), Proc1Error> {
+    println!("running as standalone pid1");
     // parse system init configuration
     // inittab is obsolated by systemd, should we honor the legacy config in that ?
     // systemd use /etc/systemd/system/default.target, which is a sym link to actual target.
@@ -55,5 +97,5 @@ fn main() -> Result<(), Error> {
             }
         }
     }
-    Ok(())
+    return Ok(());
 }
