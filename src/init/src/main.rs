fcc82235fad681c4175d68e40be0076a38531554
Ruidong Cao <caoruidong@huawei.com>
2021-10-25 19:35:07 +0800

    Add zombie reaper for init

--- src/init/src/main.rs
+++ src/init/src/main.rs
@@ -1,19 +1,16 @@
-use utils::syscall;
-
 use std::io::Error;
-use std::io::BufRead;
-use std::fs::File;
+use std::path::Path;
+use std::process::Command;
 
-use signal_hook::consts::signal::SIGCHLD;
-use signal_hook::iterator::SignalsInfo;
-use signal_hook::iterator::exfiltrator::WithOrigin;
+use nix::sys::wait;
+use nix::sys::wait::WaitPidFlag;
 
+const PROCESS1_PATH: &str = "/usr/lib/process1/process1";
+const SYSTEMD_PATH: &str = "/usr/lib/systemd/systemd";
 
 #[derive(Debug)]
 enum Proc1Error {
-    IOError(std::io::Error),
-    NoOption,
-    ChildPidTooBig(u32, std::num::TryFromIntError),
+    IOError(Error),
 }
 
 impl std::convert::From<std::io::Error> for Proc1Error {
@@ -22,80 +19,57 @@ impl std::convert::From<std::io::Error> for Proc1Error {
     }
 }
 
-fn get_command() -> Result<(String, Vec<String>), Proc1Error> {
-    let mut args = std::env::args();
-    let _me = args.next();
-    match args.next() {
-        None => Err(Proc1Error::NoOption),
-        Some(cmd) => Ok((cmd, args.collect())),
-    }
-}
-
-fn parse_inittab() -> Result<(), Error> {
-    let inittab_file = File::open("/etc/inittab")?;
-    let buf_reader = std::io::BufReader::new(inittab_file);
-    let lines = buf_reader.lines();
-
-    // actual read out configuration from /etc/inittab
-    for (num, line) in lines.enumerate() {
-        let l = line.unwrap();
-        if l.chars().nth(0).unwrap() == '#' {
-            // skip
-        } else {
-            println!("{:?}", l);
+fn main() {
+    let cmd = match get_command() {
+        Some(cmd) => cmd,
+        None => detect_init(),
+    };
+    loop {
+        if execute_mode(&cmd).is_ok() {
+            break;
         }
     }
-    //FIXME: 
-    return Ok(())
 }
 
+fn get_command() -> Option<String> {
+    std::env::args().nth(1)
+}
 
-fn main() -> Result<(), Proc1Error> {
-    match (get_command()) {
-        Ok((cmd, args)) => execute_mode(cmd, args),
-        Err(Proc1Error::NoOption) => standalone_mode(),
-        Err(_) => Ok(()),
-    };
+fn detect_init() -> String {
+    if Path::new(PROCESS1_PATH).exists() {
+        return String::from(PROCESS1_PATH);
+    }
+    String::from(SYSTEMD_PATH)
+}
 
+fn execute_mode(s: &String) -> Result<(), Proc1Error> {
+    println!("running to execute specific command {:?}", s);
+
+    let mut command = Command::new(s);
+    let mut child = command.spawn()?;
+    loop {
+        let result = child.try_wait();
+        match result {
+            Ok(Some(status)) => {
+                if status.success() {
+                    println!("successfully finished");  // shutdown
+                    break;
+                } else {
+                    println!("exited with error");
+                    child = command.spawn()?;
+                }
+            }
+            _ => {}
+        }
+        reap_zombie()
+    }
     Ok(())
 }
 
-fn execute_mode(s: String, ar: Vec<String>) -> Result<(), Proc1Error> {
-    println!("running to execute specific command");
-    println!("{:?}", s);
-    println!("{:?}", ar);
-    return Ok(())
+fn reap_zombie() {
+    let _result = wait::waitpid(nix::unistd::Pid::from_raw(-1),
+                                Some(WaitPidFlag::WNOHANG));
+    send_signal()
 }
 
-fn standalone_mode() -> Result<(), Proc1Error> {
-    println!("running as standalone pid1");
-    // parse system init configuration
-    // inittab is obsolated by systemd, should we honor the legacy config in that ?
-    // systemd use /etc/systemd/system/default.target, which is a sym link to actual target.
-    // fork process to do system initializaion
-    println!("PID: {:?}", syscall!(fork()));
-
-    match parse_inittab() {
-        Err(why) => panic!("{:?}", why),
-        Ok(config) => println!("Using /etc/inittab"),
-    };
-
-    // reacting on signls
-    // 1. collect SICHLD
-    // 2. react on service failure (restart on event)
-    // 3. handle system reboot
-    let sigs = vec![SIGCHLD];
-    
-    let mut signals = SignalsInfo::<WithOrigin>::new(&sigs)?;
-
-    for info in &mut signals {
-        println!("Received a signal {:?}", info);
-        #[cfg(feature = "extended-siginfo")]
-        match info.signal {
-            SIGCHLD => {
-                println!("CHLD received");
-            }
-        }
-    }
-    return Ok(());
-}
+fn send_signal() {}
