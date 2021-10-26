1f76d0ee3f9405b24c498d79dc1b17755ec4b4be
love_hangzhou <hushiyuan@huawei.com>
2021-10-27 02:22:21 +0800

    update dynamic lib loader

--- src/process1/src/plugin/mod.rs
+++ src/process1/src/plugin/mod.rs
@@ -2,7 +2,8 @@
 use dynamic_reload;
 use std::sync::Arc;
 use std::time::Duration;
-use std::thread;
+use walkdir::{DirEntry,WalkDir};
+use std::path::Path;
 
 struct LibLoaders{
     libs: Vec<Arc<dynamic_reload::Lib>>,
@@ -36,12 +37,79 @@ impl LibLoaders {
 
 pub struct Plugin<T> {
     plugin_lists: Vec<Arc<T>>,
-    loader_dir: Option<String>
+    library_dir: String,
+    lib_loader: LibLoaders    
 }
 
-
+#[allow(dead_code)]
 impl <T>Plugin<T> {
-    fn init() {
+    fn new() -> Self {
+        Self{
+            plugin_lists: Vec::new(),
+            library_dir: String::new(),
+            lib_loader:  LibLoaders{libs:Vec::new()},
 
+        }
+    }
+
+    pub fn set_library_dir(&mut self,library_dir:&str){
+        self.library_dir.push_str(library_dir);
+    }
+
+    pub fn is_dynamic_lib(entry: &DirEntry) -> bool{
+        let file_type = entry.file_type();
+        let file_name = entry.file_name();
+        if file_type.is_file() && file_name.to_str().map(|s| s.ends_with(".so.*")).unwrap_or(false) {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn load_lib(&mut self){
+        let file_exist = || {
+            if self.library_dir.is_empty() {
+                return false
+            }
+            if !Path::new(&self.library_dir).is_dir(){
+                return false
+            }
+            true
+        };
+        if !file_exist() {
+            return
+        }else{
+            let mut reload_handler = dynamic_reload::DynamicReload::new(Some(vec![self.library_dir.as_str()]), Some(self.library_dir.as_str()), dynamic_reload::Search::Default);
+            for entry in WalkDir::new(&self.library_dir)
+            .follow_links(true)
+            .into_iter()
+            .filter_entry(|e| Self::is_dynamic_lib(e)){
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if path.is_dir(){
+                    continue;
+                }else{
+                    let file_name = path.file_name();
+                    match file_name {
+                        Some(v) => {
+                            let str_name = v.to_str().unwrap();
+                            match reload_handler.add_library(&str_name, dynamic_reload::PlatformName::Yes){
+                                Ok(lib) =>{
+                                    println!("loader dynamic lib in to lib_loader");
+                                    self.lib_loader.add_lib(&lib);
+                                }
+                                Err(e) =>{
+                                    println!("Unable to load dynamic lib, err {:?}", e);
+                                }
+                            }
+                        }
+                        None =>{
+                            println!("file name is None");
+                        }
+                    }
+                }
+                
+            }               
+        }
     }
 }
