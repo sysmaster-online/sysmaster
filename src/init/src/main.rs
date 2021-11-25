abba593dc4b48f88a64f09acd0505b8ee5bce945
radeon92 <caoruidong@huawei.com>
2021-11-25 16:20:23 +0800

    If process1 fails 3 time in 10s, exit. And attach a patch of systemd

--- src/init/src/main.rs
+++ src/init/src/main.rs
@@ -1,29 +1,42 @@
+use std::collections::VecDeque;
 use std::io::Error;
 use std::mem::MaybeUninit;
+use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::{Command, exit};
-use std::os::unix::process::CommandExt;
+use std::time::{Duration, Instant};
 
 use libc::{siginfo_t, waitid};
 use nix::libc;
 use nix::sys::wait::waitpid;
-use nix::unistd::{Pid, fork, ForkResult};
+use nix::unistd::{fork, ForkResult, Pid};
+use signal_hook_registry::register_sigaction;
 
 const PROCESS1_PATH: &'static str = "/usr/lib/process1/process1";
 const SYSTEMD_PATH: &'static str = "/usr/lib/systemd/systemd";
+const TIME_INTERVAL: Duration = Duration::from_secs(10);
+const FAIL_COUNT: usize = 3;
 const SELF_CODE_OFFSET: i32 = 14;
 const SELF_SIG_OFFSET: i32 = 7;
 
 fn main() {
+    register_signal();
     let cmd = match get_command() {
         Some(cmd) => cmd,
         None => detect_init(),
     };
-
     loop {
         if execute_mode(&cmd).is_ok() {
             break;
         }
+    }
+}
+
+fn register_signal() {
+    unsafe {
+        let _ = register_sigaction(libc::SIGRTMIN() + SELF_SIG_OFFSET, |siginfo| {
+            let _ = waitpid(Pid::from_raw(siginfo.si_pid()), None);
+        });
     }
 }
 
@@ -46,9 +59,8 @@ fn detect_init() -> String {
 }
 
 fn execute_mode(s: &String) -> Result<(), Error> {
-    let mut error_count = 0;
+    let mut fail_record: VecDeque<Instant> = VecDeque::with_capacity(FAIL_COUNT);
     let mut child = create_init(s);
-
     loop {
         let mut siginfo = MaybeUninit::<siginfo_t>::zeroed();
         let pid = unsafe {
@@ -61,34 +73,41 @@ fn execute_mode(s: &String) -> Result<(), Error> {
         if pid <= 0 {
             continue;
         } else if pid == child.as_raw() {
-            error_count += 1;
-            if error_count >= 3 {
-                println!("Manager({}) failed 3 times, exit", pid); 
+            if need_exit(&mut fail_record) {
+                println!("Manager({}) failed 3 times, exit", pid);
                 let _ = waitpid(Pid::from_raw(pid), None);
                 break;
             }
-            child = create_init(s); 
+            child = create_init(s);
         } else {
-            send_signal(pid, siginfo); 
+            send_signal(pid, siginfo);
         }
-
-        println!("Reaped child {}", pid); 
+        println!("Reaped child {}", pid);
         let _ = waitpid(Pid::from_raw(pid), None);
     }
     Ok(())
 }
 
-fn create_init(s: &String) -> Pid  {
+fn create_init(s: &String) -> Pid {
     println!("Running to execute command : {:?}", s);
-
-    if let Ok(ForkResult::Parent { child, .. }) = unsafe{fork()} {
+    if let Ok(ForkResult::Parent { child, .. }) = unsafe { fork() } {
         child
     } else {
         let mut command = Command::new(s);
-        let comm = command.env("SYSTEMD_MAINPID", format!("{}", unsafe{ libc::getpid() }));
+        let comm = command.env("SYSTEMD_MAINPID", format!("{}", unsafe { libc::getpid() }));
         comm.exec();
         exit(0)
     }
+}
+
+fn need_exit(fail_record: &mut VecDeque<Instant>) -> bool {
+    fail_record.push_back(Instant::now());
+    if fail_record.len() == FAIL_COUNT {
+        if fail_record.pop_front().unwrap().elapsed() <= TIME_INTERVAL {
+            return true;
+        }
+    }
+    false
 }
 
 fn send_signal(pid: i32, mut siginfo: MaybeUninit<siginfo_t>) {
