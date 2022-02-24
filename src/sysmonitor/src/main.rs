de0f67549af0bc85eac35a26945825a17ef6c4df
Ruidong Cao <caoruidong@huawei.com>
2022-02-25 02:16:28 +0800

    Add sysmonitor framework
    Signed-off-by: Ruidong Cao <caoruidong@huawei.com>

---
+++ src/sysmonitor/src/main.rs
@@ -0,0 +1,123 @@
+use std::default::Default;
+use std::fs;
+use std::fs::File;
+use std::io::{self, Error, Read};
+use std::num::ParseIntError;
+use std::string::FromUtf8Error;
+
+use procfs::ProcError;
+use serde_derive::Deserialize;
+
+use crate::process::ProcessMonitor;
+use crate::process_count::ProcessCount;
+use crate::process_fd::ProcessFd;
+use crate::zombie::ZombieCount;
+
+mod process_count;
+mod zombie;
+mod process_fd;
+mod process;
+
+#[derive(Debug)]
+pub enum SysMonitorError {
+    ProcfsError(ProcError),
+    IOError(Error),
+    ParseError(ParseIntError),
+    UtfError(FromUtf8Error),
+}
+
+impl std::convert::From<ProcError> for SysMonitorError {
+    fn from(e: ProcError) -> Self {
+        SysMonitorError::ProcfsError(e)
+    }
+}
+
+impl std::convert::From<Error> for SysMonitorError {
+    fn from(e: Error) -> Self {
+        SysMonitorError::IOError(e)
+    }
+}
+
+impl std::convert::From<ParseIntError> for SysMonitorError {
+    fn from(e: ParseIntError) -> Self {
+        SysMonitorError::ParseError(e)
+    }
+}
+
+impl std::convert::From<FromUtf8Error> for SysMonitorError {
+    fn from(e: FromUtf8Error) -> Self {
+        SysMonitorError::UtfError(e)
+    }
+}
+
+const CONFIG_FILE_PATH: &str = "/etc/sysconfig/sysmonitor";
+
+pub trait Monitor {
+    fn config_path(&self) -> &str;
+    fn load(&mut self, content: String, sysmonitor: SysMonitor);
+    fn is_valid(&self) -> bool;
+    fn check_status(&mut self) -> Result<(), SysMonitorError>;
+    fn report_alarm(&self);
+}
+
+#[derive(Clone, Debug, Default, Deserialize)]
+#[serde(default, rename_all = "UPPERCASE")]
+pub struct SysMonitor {
+    #[serde(default = "on")]
+    process_monitor: bool,
+    #[serde(default = "process_monitor_period_default")]
+    process_monitor_period: u64,
+    #[serde(default = "process_recall_default_period")]
+    process_recall_period: u32,
+    #[serde(default = "process_restart_default_timeout")]
+    process_restart_timeout: u32,
+    #[serde(default = "process_alarm_suppress_num_default")]
+    process_alarm_suppress_num: u32,
+    process_alarm: bool,
+    #[serde(default = "on")]
+    pscnt_monitor: bool,
+    pscnt_alarm: bool,
+    #[serde(default = "on")]
+    process_fd_num_monitor: bool,
+    process_fd_num_alarm: bool,
+    zombie_monitor: bool,
+    zombie_alarm: bool,
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub struct Switch {
+    monitor: bool,
+    alarm: bool,
+}
+
+pub fn config_file_load(file_path: &str) -> io::Result<String> {
+    let mut file = File::open(file_path)?;
+
+    let mut buf = String::new();
+    file.read_to_string(&mut buf)?;
+    Ok(buf)
+}
+
+fn on() -> bool { true }
+
+pub fn process_monitor_period_default() -> u64 { 3 }
+
+fn process_recall_default_period() -> u32 { 1 }
+
+fn process_restart_default_timeout() -> u32 { 90 }
+
+fn process_alarm_suppress_num_default() -> u32 { 5 }
+
+fn main() -> io::Result<()> {
+    let toml_str = fs::read_to_string(CONFIG_FILE_PATH)?;
+    let sysmonitor: SysMonitor = toml::from_str(&toml_str).unwrap();
+
+    let monitors: [&mut dyn Monitor; 4] = [&mut ProcessCount::default(),&mut ZombieCount::default(),&mut ProcessFd::default(), &mut ProcessMonitor::default()];
+    for monitor in monitors {
+        let contents = fs::read_to_string(monitor.config_path())?;
+        monitor.load(contents, sysmonitor.clone());
+        monitor.is_valid();
+        monitor.check_status();
+    }
+    Ok(())
+}
---
+++ src/sysmonitor/src/process.rs
@@ -0,0 +1,165 @@
+use std::io;
+use std::os::unix::process::CommandExt;
+use std::process::{Child, Command, ExitStatus};
+use std::thread::sleep;
+use std::time::Duration;
+
+use nix::libc::pid_t;
+use nix::sys::signal::kill;
+use nix::sys::signal::Signal;
+use nix::unistd::Pid;
+use serde_derive::Deserialize;
+
+use crate::{Monitor, Switch, SysMonitor, SysMonitorError};
+use crate::process_monitor_period_default;
+
+const CONFIG_FILE_PATH: &str = "/etc/sysmonitor/process";
+const PROCESS_EXIT_TIMEOUT: u64 = 10;
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub struct ProcessMonitor {
+    pub(crate) config: Switch,
+    pub name: String,
+    #[serde(default)]
+    recover_command: String,
+    #[serde(default)]
+    monitor_command: String,
+    #[serde(default)]
+    stop_command: String,
+    #[serde(default)]
+    uid: u32,
+    #[serde(default)]
+    check_as_param: bool,
+    #[serde(default = "monitor_mode_default")]
+    monitor_mode: String,
+    #[serde(default = "process_monitor_period_default")]
+    monitor_period: u64,
+    #[serde(default)]
+    usr_cmd_alarm: bool,
+    #[serde(default)]
+    alarm_command: String,
+    #[serde(default)]
+    alarm_recover_command: String,
+    #[serde(default)]
+    timeout: u64,
+}
+
+fn monitor_mode_default() -> String { "serial".to_string() }
+
+impl Monitor for ProcessMonitor {
+    fn config_path(&self) -> &str {
+        CONFIG_FILE_PATH
+    }
+
+    fn load(&mut self, content: String, sysmonitor: SysMonitor) {
+        let monitor: Self = toml::from_str(content.as_str()).unwrap();
+        *self = ProcessMonitor {
+            config: Switch {
+                monitor: sysmonitor.pscnt_monitor,
+                alarm: sysmonitor.pscnt_alarm,
+            },
+            ..monitor
+        };
+    }
+
+    fn is_valid(&self) -> bool {
+        (self.monitor_mode == "serial" || self.monitor_mode == "parallel") && self.monitor_period > 0
+    }
+
+    fn check_status(&mut self) -> Result<(), SysMonitorError> {
+        self.process_monitor_start(self.timeout);
+        Ok(())
+    }
+
+    fn report_alarm(&self) {}
+}
+
+impl ProcessMonitor {
+    fn process_monitor_start(&mut self, timeout: u64) {
+        loop {
+            self.reload_tasks();
+            if self.check_service_exist() {
+                self.process_recover(timeout);
+                self.process_alarm();
+            }
+            sleep(Duration::from_secs(self.monitor_period))
+        }
+    }
+
+    fn reload_tasks(&mut self) {}
+
+    fn get_process_check_timeout(&self) -> u64 {
+        self.monitor_period + 344
+    }
+
+    fn check_service_exist(&mut self) -> bool {
+        for _ in 1..2 {
+            match self.check_process_exist() {
+                Ok(true) => return true,
+                Ok(false) => continue,
+                Err(_) => {
+                    return false;
+                }
+            }
+        }
+        false
+    }
+
+    fn check_process_exist(&mut self) -> io::Result<bool> {
+        command_wait(self.monitor_command.clone(), self.stop_command.clone(), self.uid, self.timeout)
+    }
+
+    fn process_recover(&self, timeout: u64) -> io::Result<bool> {
+        command_wait(self.recover_command.clone(), self.stop_command.clone(), self.uid, timeout)
+    }
+
+    fn process_alarm(&mut self) -> io::Result<bool> {
+        command_wait(self.alarm_command.clone(), "".to_string(), self.uid, self.timeout)
+    }
+
+    fn process_alarm_recover(self) -> io::Result<bool> {
+        command_wait(self.alarm_recover_command, "".to_string(), self.uid, self.timeout)
+    }
+}
+
+fn command_wait(command: String, stop_command: String, uid: u32, timeout: u64) -> io::Result<bool> {
+    let mut child = Command::new(command).uid(uid).spawn()?;
+
+    if timeout > 0 {
+        wait_child(&mut child, timeout, 100);
+        if stop_command != "" {
+            let _stop = Command::new(stop_command).uid(uid).output();
+            process_exit(&mut child);
+        }
+    } else {
+        return match child.wait() {
+            Ok(status) => { Ok(status.success()) }
+            Err(e) => { Err(e) }
+        };
+    }
+
+    Ok(false)
+}
+
+fn process_exit(child: &mut Child) -> io::Result<ExitStatus> {
+    kill(Pid::from_raw(child.id() as pid_t), Signal::SIGTERM);
+    sleep(Duration::from_secs(1));
+    wait_child(child, PROCESS_EXIT_TIMEOUT, 1000);
+    child.kill();
+    child.wait()
+}
+
+fn wait_child(child: &mut Child, timeout_s: u64, sleep_ms: u64) -> io::Result<Option<ExitStatus>> {
+    for _i in 0..timeout_s * 1000 / sleep_ms {
+        match child.try_wait() {
+            Ok(None) => {
+                sleep(Duration::from_millis(sleep_ms));
+            }
+            r => {
+                return r;
+            }
+        };
+    }
+    Ok(None)
+}
