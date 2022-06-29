fe251aeb983a22b5454a52f517afa55646cf217d
fangchuangchuang <fangchuangchuang@huawei.com>
2022-06-29 11:04:04 +0800

    新增target组件功能支持

---
+++ components/target/src/target_comm.rs
@@ -0,0 +1,48 @@
+/* 关联unit对象
+*需要通知Unit对象，方法变更
+*获取unit对象的属性
+*调用关系
+*target_unit->target_mng->target_comm
+*/
+
+use process1::manager::Unit;
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
+
+pub(super) struct TargetComm {
+    data: RefCell<TargetCommData>,
+}
+
+impl TargetComm {
+    pub(super) fn new() -> Self {
+        TargetComm {
+            data: RefCell::new(TargetCommData::new()),
+        }
+    }
+    pub(super) fn attach_unit(&self, unit: Rc<Unit>) {
+        self.data.borrow_mut().attach_unit(unit);
+    }
+
+    pub(super) fn unit(&self) -> Option<Rc<Unit>> {
+        self.data.borrow().unit()
+    }
+}
+struct TargetCommData {
+    unit: Weak<Unit>,
+}
+
+impl TargetCommData {
+    pub(self) fn new() -> TargetCommData {
+        TargetCommData { unit: Weak::new() }
+    }
+
+    fn attach_unit(&mut self, unit: Rc<Unit>) {
+        self.unit = Rc::downgrade(&unit);
+    }
+
+    pub(self) fn unit(&self) -> Option<Rc<Unit>> {
+        self.unit.clone().upgrade()
+    }
+}
