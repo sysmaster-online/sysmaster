7609f0152411346db7ddd401fc706552b727c161
love_hangzhou <hushiyuan@huawei.com>
2022-08-18 00:49:19 +0800

    add condition support

---
+++ libutils/src/condition.rs
@@ -0,0 +1,134 @@
+use std::path::Path;
+
+pub enum ConditionType {
+    PathExists,
+    FileNotEmpty,
+    _MAX,
+}
+
+// condtion 判断条件是否满足，如果条件以|开头，表示触发条件，触发条件只要有一个满足，则可以了
+//条件以!开始表示反转，
+//其他，表示普通条件
+pub struct Condition {
+    c_type: ConditionType,
+    trigger: i8,
+    revert: i8,
+    params: String,
+}
+
+impl Condition {
+    pub fn new(_type: ConditionType, trigger: i8, revert: i8, _params: String) -> Self {
+        Condition {
+            c_type: _type,
+            trigger: trigger,
+            revert: revert,
+            params: _params,
+        }
+    }
+    pub fn trigger(&self) -> i8 {
+        self.trigger
+    }
+
+    pub fn revert(&self) -> i8 {
+        self.revert
+    }
+
+    pub fn test(&self) -> bool {
+        if self.params.is_empty() {
+            return false;
+        }
+        let mut result = match self.c_type {
+            ConditionType::PathExists => self.test_path_exists(),
+            ConditionType::FileNotEmpty => self.test_file_not_empty(),
+            ConditionType::_MAX => todo!(),
+        };
+        if self.revert() >= 1 {
+            result = !result;
+        }
+
+        if result > 0 {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn test_path_exists(&self) -> i8 {
+        let tmp_path = Path::new(&self.params);
+        let result = tmp_path.exists();
+        if result {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    fn test_file_not_empty(&self) -> i8 {
+        let tmp_path = Path::new(&self.params);
+        let result = tmp_path
+            .metadata()
+            .map(|m| {
+                if m.is_file() {
+                    m.len() > 0
+                } else {
+                    return false;
+                }
+            })
+            .unwrap_or(false);
+        if result {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::logger;
+
+    use super::{Condition, ConditionType};
+    use crate::test_util::get_project_root;
+
+    #[test]
+    fn test_condition_test() {
+        logger::init_log_with_console("test_init_lookup_paths", 4);
+        let project_root = get_project_root().unwrap();
+        let cond_path_not_exists =
+            Condition::new(ConditionType::PathExists, 0, 0, "/home/test".to_string());
+        let f_result = cond_path_not_exists.test();
+        assert!(f_result == false);
+        log::debug!("project root {:?}", project_root);
+        let cond_path_exists = Condition::new(
+            ConditionType::PathExists,
+            0,
+            0,
+            project_root.to_str().unwrap().to_string(),
+        );
+        let t_result = cond_path_exists.test();
+        assert!(t_result, "condtion_path exists is not true");
+        let cond_path_exists_revert = Condition::new(
+            ConditionType::PathExists,
+            0,
+            1,
+            project_root.to_str().unwrap().to_string(),
+        );
+        let f_result = cond_path_exists_revert.test();
+        assert!(f_result == false, "condtion test path exist revert error");
+        let cond_file_not_empty = Condition::new(
+            ConditionType::FileNotEmpty,
+            0,
+            0,
+            project_root.to_str().unwrap().to_string() + "/Cargo.lock",
+        );
+        assert!(cond_file_not_empty.test(), "cond test file not empty");
+
+        let cond_file_empty = Condition::new(
+            ConditionType::FileNotEmpty,
+            0,
+            0,
+            project_root.to_str().unwrap().to_string(),
+        );
+        assert!(!cond_file_empty.test(), "cond test file empty");
+    }
+}
