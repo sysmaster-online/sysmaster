6f120c4b888ce7a2737f805c12eb249e2fb614ea
overweight <hexiaowen@huawei.com>
2022-09-26 19:27:08 +0800

    add ci check for missing_docs and use chinese char in source code

--- ci/01-pre-commit.sh
+++ ci/01-pre-commit.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # try install 3 times
 pip3 install pre-commit -i http://mirrors.aliyun.com/pypi/simple/ || pip3 install  -i https://pypi.tuna.tsinghua.edu.cn/simple/ pre-commit || pip3 install pre-commit
-#rustlist=`git diff --name-only HEAD~5 HEAD | grep \.rs$ | tr '\n' ' '`
-#grep -P '[\p{Han}]' $rustlist && echo "rust 源码文件中禁用中文字符" && exit
-filelist=`git diff --name-only HEAD~20 HEAD | tr '\n' ' '`
+
+## one PR ? Commit
+oldnum=`git rev-list origin/master --count`
+newnum=`git rev-list HEAD --count`
+changenum=$[newnum - oldnum]
+
+# add doc for src code
+sed -i '1i\#![deny(missing_docs)]' $rustlist
+
+# run base check
+filelist=`git diff --name-only HEAD~$changenum HEAD | tr '\n' ' '`
 export PATH="$PATH:/home/jenkins/.local/bin"
 pre-commit run -vvv --files ${filelist}
