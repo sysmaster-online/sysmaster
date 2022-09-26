dfeeeba6688e7481cd3b88c83b0e2f23376908b5
overweight <hexiaowen@huawei.com>
2022-09-26 17:08:32 +0800

    Use a faster source for pip

--- ci/01-pre-commit.sh
+++ ci/01-pre-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # try install 3 times
-pip3 install pre-commit || pip3 install pre-commit || pip3 install pre-commit
+pip3 install pre-commit -i http://mirrors.aliyun.com/pypi/simple/ || pip3 install  -i https://pypi.tuna.tsinghua.edu.cn/simple/ pre-commit || pip3 install pre-commit
 #rustlist=`git diff --name-only HEAD~5 HEAD | grep \.rs$ | tr '\n' ' '`
 #grep -P '[\p{Han}]' $rustlist && echo "rust 源码文件中禁用中文字符" && exit
 filelist=`git diff --name-only HEAD~20 HEAD | tr '\n' ' '`
