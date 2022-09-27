f64550175688ea33e1aa4d6c8c556a788440875c
overweight <hexiaowen@huawei.com>
2022-09-27 15:04:35 +0800

    deny clippy warnings and build warnings

--- ci/01-pre-commit.sh
+++ ci/01-pre-commit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # try install 3 times
+
+trap 'echo "--- PLEASE RUN sh -x ci/01-pre-commit.sh FIRST IN YOUR LOCALHOST!!! ---"' EXIT
 pip3 install pre-commit -i http://mirrors.aliyun.com/pypi/simple/ || pip3 install  -i https://pypi.tuna.tsinghua.edu.cn/simple/ pre-commit || pip3 install pre-commit
 
 ## one PR ? Commit
@@ -8,8 +10,12 @@ newnum=`git rev-list HEAD --count`
 changenum=$[newnum - oldnum]
 
 # add doc for src code
-rustlist=`git diff --name-only HEAD~$changenum HEAD | grep \.rs$ | tr '\n' ' '`
-sed -i '1i\#![deny(missing_docs)]' $rustlist 2>/dev/null || true
+for rustlist in `git diff --name-only HEAD~$changenum HEAD | grep \.rs$ | tr '\n' ' '`
+do
+egrep '#!\[deny\(missing_docs\)\]' $rustlist || sed -i '1i\#![deny(missing_docs)]' $rustlist 2>/dev/null || true
+egrep '#!\[deny\(clippy::all\)\]' $rustlist || sed -i '1i\#![deny(clippy::all)]' $rustlist 2>/dev/null || true
+egrep '#!\[deny\(warnings\)\]' $rustlist || sed -i '1i\#![deny(warnings)]' $rustlist 2>/dev/null || true
+done
 
 # run base check
 filelist=`git diff --name-only HEAD~$changenum HEAD | tr '\n' ' '`
