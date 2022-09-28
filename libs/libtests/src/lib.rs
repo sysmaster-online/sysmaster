2b46dccf6a43fc32ef16d61e6afeb7d06c7ace46
overweight <hexiaowen@huawei.com>
2022-09-28 20:15:39 +0800

    clean warning for libtests

--- libs/libtests/src/lib.rs
+++ libs/libtests/src/lib.rs
@@ -1,6 +1,5 @@
 use std::{
     env,
-    ffi::OsString,
     fs::read_dir,
     io::{self, ErrorKind},
     path::PathBuf,
@@ -8,12 +7,11 @@ use std::{
 
 pub fn get_project_root() -> io::Result<PathBuf> {
     let path = env::current_dir()?;
-    let mut path_ancestors = path.as_path().ancestors();
 
-    while let Some(p) = path_ancestors.next() {
+    for p in path.as_path().ancestors() {
         let has_cargo = read_dir(p)?
             .into_iter()
-            .any(|p| p.unwrap().file_name() == OsString::from("Cargo.lock"));
+            .any(|p| p.unwrap().file_name().eq("Cargo.lock"));
         if has_cargo {
             return Ok(PathBuf::from(p));
         }
