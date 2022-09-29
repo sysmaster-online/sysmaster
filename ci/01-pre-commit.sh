592955a9d12240cf1c41dcf87ff10f5f6344e5d1
overweight <hexiaowen@huawei.com>
2022-09-29 10:45:30 +0800

    fix cgroup test fail in non-root

--- ci/01-pre-commit.sh
+++ ci/01-pre-commit.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # try install 3 times
 
-trap 'echo "--- PLEASE RUN sh -x ci/01-pre-commit.sh FIRST IN YOUR LOCALHOST!!! ---"' EXIT
+function finish() {
+    echo "--- PLEASE RUN sh -x ci/01-pre-commit.sh FIRST IN YOUR LOCALHOST!!! ---"
+    # remove tmp
+    for rustlist in `git diff --name-only HEAD~$changenum | grep \.rs$ | tr '\n' ' '`
+    do
+    sed -i '/#!\[deny(missing_docs)]/d' $rustlist 2>/dev/null || true
+    sed -i '/#!\[deny(clippy::all)]/d' $rustlist 2>/dev/null || true
+    sed -i '/#!\[deny(warnings)]/d' $rustlist 2>/dev/null || true
+    done
+}
+
+trap finish EXIT
 pip3 install pre-commit -i http://mirrors.aliyun.com/pypi/simple/ || pip3 install  -i https://pypi.tuna.tsinghua.edu.cn/simple/ pre-commit || pip3 install pre-commit
 
 ## one PR ? Commit
@@ -21,3 +32,12 @@ done
 filelist=`git diff --name-only HEAD~$changenum HEAD | tr '\n' ' '`
 export PATH="$PATH:/home/jenkins/.local/bin"
 pre-commit run -vvv --files ${filelist}
+
+
+# remove tmp
+for rustlist in `git diff --name-only HEAD~$changenum | grep \.rs$ | tr '\n' ' '`
+do
+sed -i '/#!\[deny(missing_docs)]/d' $rustlist 2>/dev/null || true
+sed -i '/#!\[deny(clippy::all)]/d' $rustlist 2>/dev/null || true
+sed -i '/#!\[deny(warnings)]/d' $rustlist 2>/dev/null || true
+done
