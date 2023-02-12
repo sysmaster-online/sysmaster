d7b41d7198f0181c099fc69ededea25181ec411c
fangchuang <fangchuangchuang@huawei.com>
2023-02-12 10:20:23 +0800

    testcases: Add testcases for apply_sock_opt
    1. Add testcases for apply_sock_opt
    2. Fix the error caused by incorrect test.socket.toml path

--- coms/socket/src/socket_load.rs
+++ coms/socket/src/socket_load.rs
@@ -140,7 +140,7 @@ mod tests {
     fn test_socket_load_parse() {
         let comm = Rc::new(SocketUnitComm::new());
         let mut file_path = get_project_root().unwrap();
-        file_path.push("test_units/test.socket.toml");
+        file_path.push("tests/test_units/test.socket.toml");
 
         let paths = vec![file_path];
 
