78b22e5caedb33659c8a5d518c063b7c8f0fdb8f
oh_thats_great <hexinyue1@huawei.com>
2023-04-27 20:49:57 +0800

    testcase: add daemon-reload reliable test

---
+++ tests/common/monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while ((1)); do
+    time="$(date)"
+    init_data="$(ps aux | grep -v grep | grep '/usr/bin/init')"
+    sysmst_data="$(ps aux | grep -v grep | grep '/usr/lib/sysmaster/sysmaster')"
+
+    pid=$(echo ${init_data} | awk '{print $2}')
+    rss=$(echo ${init_data} | awk '{print $6}')
+    line="${time}     init(pid:${pid}  rss:$rss  fd:$(ls /proc/${pid}/fd | wc -l))"
+    pid=$(echo ${sysmst_data} | awk '{print $2}')
+    rss=$(echo ${sysmst_data} | awk '{print $6}')
+    line="${line}     sysmaster(pid:${pid}  rss:$rss  fd:$(ls /proc/${pid}/fd | wc -l))"
+    echo "${line}" >> /opt/monitor.log
+    sleep 60
+done
