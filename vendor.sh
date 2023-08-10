09b0da8da4939193862324ba8097ce882dde555e
overweight <hexiaowen@huawei.com>
2023-08-10 17:08:40 +0800

    build: make vendor.sh happy

--- vendor.sh
+++ vendor.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+# prepare environment
+for i in `ls ci/*.sh | sort -u -d | grep "00-pre.sh" `
+do
+    date; sh -x -e $i;
+done
+
+# cargo vendor
 echo "cargo vendor ..."
 rustup override set stable
 rm -rf vendor
 cargo vendor
 rustup override unset
 
-echo "set vendor replace crates.io ..."
+echo "set replace crates.io in .cargo/config ..."
 grep -i "vendored-sources" .cargo/config
 if [ $? -ne 0 ]; then
 cat << EOF >> .cargo/config
@@ -19,19 +26,29 @@ directory = "vendor"
 EOF
 fi
 
+
+
 echo "Applying patches in patch directory ..."
 for i in `ls patch/*.patch | sort -u -d`
 do
-    git am $i
+    git am $i;
 done
 
 echo "Cargo build ..."
 for i in `ls ci/*.sh | sort -u -d | grep -v "00-pre.sh" `
 do
-    date; sh -x -e $i;
+    date; sh -x $i;
 done
 
+# cleanup temporary
+cargo clean
+git checkout -- .cargo/config
+
+# echo sysMaster version
 echo "Create a compressed archive of tar.gz ..."
-version_line=$(grep -Eo 'version = "[0-9]+\.[0-9]+\.[0-9]+"' ./Cargo.toml)
+version_line=$(grep -Eo '^version = "[0-9]+\.[0-9]+\.[0-9]+"' ./Cargo.toml)
 version=$(echo "$version_line" | awk -F'"' '{print $2}')
-echo "You can create sysmaster-$version.tar.gz by using the tar -zcvf command."
+echo "\n\n\nYou can create sysmaster-$version.tar.gz by using the tar -zcvf command."
+
+echo "You can replace crates.io with vendored-sources in .cargo/config!!!"
+cat .cargo/config
