0b691c0dbf9d3abc1d76a45752874526e2e9818a
liaoqingwei <liaoqingwei@huawei.com>
2023-11-13 21:03:40 +0800

    docs: add image build with kiwi

--- docs/cicd/build_rpm_with_mock/build_rpm.sh
+++ docs/cicd/build_rpm_with_mock/build_rpm.sh
@@ -2,12 +2,29 @@
 set -e
 arch=$(uname -m)
 vendor="openeuler-22.03LTS_SP1"
-if [ $# -ne 1 ]; then
-    echo "More than one argument supplied, not supported"
-    echo "./build_rpm.sh [openeuler-22.03LTS_SP1]"
-else
-    vendor=$1
-fi
+force=false
+
+# 处理参数
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        --force)
+        force=true
+        shift # 移除参数
+        ;;
+        *)
+        if [ $# -ne 1 ]; then
+            echo "More than one argument supplied, not supported"
+            echo "./build_rpm.sh [openeuler-22.03LTS_SP1]"
+            exit 1
+        else
+            vendor=$1
+        fi
+        shift # 移除参数
+        ;;
+    esac
+done
 
 # 获取当前脚本的目录
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
@@ -18,70 +35,70 @@ while [ ! -f "$ROOTDIR/Cargo.lock" ] && [ "$ROOTDIR" != "/" ]; do
     ROOTDIR=$(dirname "$ROOTDIR")
 done
 
-TARGETDIR=$ROOTDIR/target/rpms
-rm -rf $TARGETDIR
-mkdir -p $TARGETDIR
-
 pushd $ROOTDIR
-# 利用cargo vendor创建源码压缩包
-rustup override set stable
-rm -rf vendor
-cargo vendor
-rustup override unset
-
-# delete large and unused files
-for lib in `find vendor/windows* | grep \\.a$`
-do
-    rm -rf $lib
-done
-for lib in `find vendor/winapi* | grep \\.a$`
-do
-    rm -rf $lib
-done
-for lib in `find vendor/windows* | grep \\.lib$`
-do
-    rm -rf $lib
-done
-
-rm -rf petgraph/tests
-
-for crate in `ls -d vendor/win*`
-do
-    pushd $crate/src
-    if [ $? -ne 0 ] ;then
-        continue;
-    fi
-    for pathToDelete in `ls`
-    do
-        if [ -d "$pathToDelete" ]; then
-            echo "Deleting files in $pathToDelete..."
-            rm -rf "$pathToDelete"
-        else
-            echo "$pathToDelete is not dir."
-        fi
-    done
-    popd
-done
-
-echo "Create a compressed archive of tar.gz ..."
 version_line=$(grep -Eo '^version = "[0-9]+\.[0-9]+\.[0-9]+"' ./Cargo.toml)
 version=$(echo "$version_line" | awk -F'"' '{print $2}')
 
-# compress sysmaster
-pushd $ROOTDIR/../
-    rm -rf sysmaster-$version
-    cp -a $(basename $ROOTDIR) sysmaster-$version
-    pushd sysmaster-$version
-    cargo clean
-    rm -rf .git next docs tools patch target
-    sed -i '/\[patch.crates-io.loopdev\]/{N;N;d}' Cargo.toml
+TARGETDIR=$ROOTDIR/target/rpms
+if [ "$force" = false ]; then
+    rm -rf $TARGETDIR
+    mkdir -p $TARGETDIR
+# 利用cargo vendor创建源码压缩包
+    rustup override set stable
+    rm -rf vendor
+    cargo vendor
+    rustup override unset
+
+    # delete large and unused files
+    for lib in `find vendor/windows* | grep \\.a$`
+    do
+        rm -rf $lib
+    done
+    for lib in `find vendor/winapi* | grep \\.a$`
+    do
+        rm -rf $lib
+    done
+    for lib in `find vendor/windows* | grep \\.lib$`
+    do
+        rm -rf $lib
+    done
+
+    rm -rf petgraph/tests
+
+    for crate in `ls -d vendor/win*`
+    do
+        pushd $crate/src
+        if [ $? -ne 0 ] ;then
+            continue;
+        fi
+        for pathToDelete in `ls`
+        do
+            if [ -d "$pathToDelete" ]; then
+                echo "Deleting files in $pathToDelete..."
+                rm -rf "$pathToDelete"
+            else
+                echo "$pathToDelete is not dir."
+            fi
+        done
+        popd
+    done
+
+    # compress sysmaster
+    pushd $ROOTDIR/../
+        rm -rf sysmaster-$version
+        cp -a $(basename $ROOTDIR) sysmaster-$version
+        pushd sysmaster-$version
+        cargo clean
+        rm -rf .git next docs tools patch target
+        sed -i '/\[patch.crates-io.loopdev\]/{N;N;d}' Cargo.toml
+        popd > /dev/null 2>&1
+        tar -cJvf $TARGETDIR/sysmaster-$version.tar.xz sysmaster-$version
+        rm -rf sysmaster-$version
     popd > /dev/null 2>&1
-    tar -cJvf $TARGETDIR/sysmaster-$version.tar.xz sysmaster-$version
-    rm -rf sysmaster-$version
-popd > /dev/null 2>&1
+fi
 set +e
 # 构建srpm
-sudo dnf install -y mock rpm-build
+sudo dnf install -y mock rpm-build createrepo
 sudo groupadd mock
 sudo usermod -a -G mock $(who | awk '{print $1}' | sort -u)
 cp -a $SCRIPT_DIR/* $TARGETDIR
@@ -90,3 +107,5 @@ mock -r $vendor-$arch --configdir $TARGETDIR --no-clean --isolation simple --bui
 # rebuild构建rpms, 结果输出到target/rpms目录下
 srpms=$(ls $TARGETDIR/sysmaster-*.src.rpm)
 mock -r $vendor-$arch --configdir $TARGETDIR --no-clean --isolation simple --rebuild  $srpms --resultdir $TARGETDIR
+createrepo_c $TARGETDIR
+popd
