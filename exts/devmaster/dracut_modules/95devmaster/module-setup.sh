7b40c95a548690313ac3e27eb23b529f6cc2585c
huskar <tanly6@chinatelecom.cn>
2024-01-03 09:30:48 +0800

    feature(devmaster): add dracut module to install devmaster in initrd
    The init.sh will replace that from 99base dracut module.

---
+++ exts/devmaster/dracut_modules/95devmaster/module-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+check() {
+    require_binaries devctl || return 1
+    return 0
+}
+
+# called by dracut
+install() {
+    inst_script "$moddir/init.sh" "/init"
+
+    inst_multiple devctl
+
+    inst_dir /etc/devmaster
+    inst_dir /lib/devmaster
+    inst_dir /etc/devmaster/network.d
+    inst_multiple -o /etc/devmaster/config.toml \
+	    /etc/devmaster/network.d/99-default.link \
+	    /lib/devmaster/simulate_udev.sh
+    ln -sf /bin/devctl "${initdir}"/bin/udevadm
+    ln -sf /bin/devctl "${initdir}"/lib/devmaster/devmaster
+}
